a919ed852c7a796b199308ff323a1177784df09a
Daniel Terranova <daniel.terranova@gmail.com>
2014-06-01 16:48:29 +0200

    more calendar

--- cpp-code/date/includes/calendar.h
+++ cpp-code/date/includes/calendar.h
@@ -7,181 +7,212 @@
 #include <list>
 
 namespace lab2 {
-    template<typename C>
-    class Calendar
-        {
-          public:
-          Calendar() : events ( std::list<Calendar::Event>{} ) {
-          }
-            
-            friend std::ostream& operator<<(std::ostream& ostream,
-                                            const Calendar<C>& d ) {
-                typename std::list<Calendar<C>::Event>::const_iterator it =
-                    d.events.begin();
-                for ( ; it != d.events.end() ; it++ ) {
-                    Calendar<C>::Event event = *it;
-                    // if (event.event_date() > d.current_date ) {
-                    ostream << event << std::endl;
-                    // }
-                }
-                return ostream;
-            }
+  template<typename C>
+  class Calendar
+  {
+  public:
+    Calendar() : events ( std::list<Calendar::Event>{} ) {
+    }
+    // set the current date for this calendar.
+    bool set_date (int year, int month , int day ) {
+      C date;
+      try {
+        date = C(year,month,day);
+      } catch (std::out_of_range ignore ) {
+        return false;
+      }
+      current_date = date;
+      return true;
+    }
+
+    C get_date() {
+      return current_date;
+    }
+
+    bool add_event ( std::string str , C date ) {
+      typename std::list<Calendar<C>::Event>::iterator it =
+        events.begin();
+
+      for (; it != events.end(); it++ ) {
+        Calendar<C>::Event event = *it;
+        if ( event.event_date() == date &&
+             event.event_str() == str )
+          return false;
+        else if (event.event_date() > date ) {
+          events.insert ( it , Event ( date , str ) );
+          return true;
+        }
+      }
+      Calendar<C>::Event newEvent( date, str);
+      std::cout << " add event :" << newEvent << std::endl;
+      events.push_back ( newEvent );
+      std::cout << " events.size() :" << events.size() << std::endl;
+      return true;
+    }
+
+    bool add_event(std::string str , int day) {
+      C event_date;
+      try {
+        event_date =
+          C(current_date.year(), current_date.month(), day);
+        return add_event ( str, event_date );
+      } catch ( std::out_of_range ign ) {
+        return false;
+      }
+    }
+
+    bool add_event(std::string str , int day , int month ) {
+      C event_date;
+      try {
+        event_date =
+          C(current_date.year(), month , day);
+        return add_event ( str, event_date );
+      } catch ( std::out_of_range ign ) {
+        return false;
+      }
+    }
+
+    bool add_event(std::string str , int day , int month , int year ) {
+      C event_date;
+      try {
+        event_date =
+          C(year, month , day);
+        return add_event ( str, event_date );
+      } catch ( std::out_of_range ign ) {
+        return false;
+      }
+    }
+
+    bool remove_event (std::string str, int day ) {
+      C event_date;
+      try {
+        event_date = C(current_date.year(), current_date.month(),
+                       day );
+        return remove_event ( str , event_date ) ;
+      } catch ( std::out_of_range ign ) {
+        return false;
+      }
+    }
+
+    bool remove_event (std::string str , int month , int day ) {
+      C event_date;
+      try {
+        event_date = C(current_date.year(), month,
+                       day );
+        return remove_event ( str , event_date ) ;
+      } catch ( std::out_of_range ign ) {
+        return false;
+      }
+    }
+
+    bool remove_event (std::string str , int year , int month , int day )
+    {
+      C event_date;
+      try {
+        event_date = C(year, month,
+                       day );
+        return remove_event ( str , event_date ) ;
+      } catch ( std::out_of_range ign ) {
+        return false;
+      }
+    }
+
+    bool remove_event (std::string str , C date ) {
+      typename std::list<Calendar<C>::Event>::iterator it =
+        events.begin();
+      for (; it != events.end(); it++ ) {
+        Calendar<C>::Event event = *it;
+        if ( event.event_date() == date
+             && event.event_str() == str ) {
+          events.remove ( event );
+          return true;
+        }
+      }
+      return false;
+    }
 
 
-          bool set_date (int year, int month , int day ) {
-            std::cout << "set date  " << std::endl;
-            C date;
-              try {
-                date = C(year,month,day);
-                std::cout << "date  " << date << std::endl;
-              } catch (std::out_of_range ignore ) {
-                return false;
-              }
-              current_date = date;
-              return true;
-            }
-          
-          C get_date() {
-            return current_date;
-          }
+    // inner class --
+    class Event {
+    public:
+      Event (C date, std::string s): date (date), str(s) { }
 
-          
+      bool operator==(const Event& event) {
+        if (event.event_date() == date &&
+            event.event_str() ==  str ) {
+          return true;
+        }
+        return false;
+      }
+      // Less than operator: compares date
+      // if date are equal then compare event string
+      bool operator <(const Event& e) const {
+        if (event_date() < e.event_date()) {
+          return true;
+        } else if ( event_date() > e.event_date() ) {
+          return false;
+        } else {  // equal
+          return ( event_str() < e.event_str() );
+        }
+      }
+      
 
-          bool add_event ( std::string str , C date ) {
-            typename std::list<Calendar<C>::Event>::iterator it =
-              events.begin();
-            for (; it != events.end(); it++ ) {
-              Calendar<C>::Event event = *it;
-              if ( event.event_date() == date &&
-                   event.event_str() == str )
-                return false;
-              else if (event.event_date() > date ) {
-                events.insert ( it , Event ( date , str ) );
-                return true;
-              }
-            }
-            events.push_back ( Calendar<C>::Event ( date , str ));
-            return true;
-          }
-          
-          bool add_event(std::string str , int day) {
-            C event_date;
-            try {
-              event_date =
-                C(current_date.year(), current_date.month(), day);
-              return add_event ( str, event_date );
-            } catch ( std::out_of_range ign ) {
-              return false;
-            }
-          }
 
-          bool add_event(std::string str , int day , int month ) {
-            C event_date;
-            try {
-              event_date =
-                C(current_date.year(), month , day);
-              return add_event ( str, event_date );
-            } catch ( std::out_of_range ign ) {
-              return false;
-            }
-          }
+      friend std::ostream& operator<<(std::ostream& ostream,
+                                      const Event& e )
+      {
+        ostream << e.event_date() << " : " << e.event_str();
+        return ostream;
+      }
+      C event_date() const {
+        return date;
+      }
+      std::string event_str() const {
+        return str;
+      }
 
-          bool add_event(std::string str , int day , int month , int year ) {
-            std::cout << "ADD event " << std::endl;
-            C event_date;
-            try {
-              event_date =
-                C(year, month , day);
-              std::cout << "event_date :" << event_date << std::endl;
-              return add_event ( str, event_date );
-            } catch ( std::out_of_range ign ) {
-              return false;
-            }
-          }
-
-          bool remove_event (std::string str, int day ) {
-            C event_date;
-            try {
-              event_date = C(current_date.year(), current_date.month(),
-                             day );
-              return remove_event ( str , event_date ) ;
-            } catch ( std::out_of_range ign ) {
-              return false;
-            }
-          }
-
-          bool remove_event (std::string str , int month , int day ) {
-            C event_date;
-            try {
-              event_date = C(current_date.year(), month,
-                             day );
-              return remove_event ( str , event_date ) ;
-            } catch ( std::out_of_range ign ) {
-              return false;
-            }
-          }
-
-          bool remove_event (std::string str , int year , int month , int day )
-          {
-            C event_date;
-            try {
-              event_date = C(year, month,
-                             day );
-              return remove_event ( str , event_date ) ;
-            } catch ( std::out_of_range ign ) {
-              return false;
-            }
-          }
-
-          bool remove_event (std::string str , C date ) {
-            typename std::list<Calendar<C>::Event>::iterator it =
-              events.begin();
-            for (; it != events.end(); it++ ) {
-              Calendar<C>::Event event = *it;
-              if ( event.event_date() == date
-                   && event.event_str() == str ) {
-                events.remove ( event );
-                return true;
-              }
-            }
-            return false;
-          }
-          // inner class --
-          class Event {
-          public:
-            Event (C date, std::string s): date (date), str(s) { }
-
-            bool operator==(const Event& event) {
-              if (event.event_date() == date && 
-                  event.event_str() ==  str ) {
-                return true;
-              }
-              return false;
-            }
-            friend std::ostream& operator<<(std::ostream& ostream,
-                                            const Event& e )
-            {
-              ostream << e.event_date() << ": " << e.event_str();
-              return ostream;
-            }
-            C event_date() const { 
-              return date; 
-            }
-            std::string event_str() const { 
-              return str; 
-            }
-
-          private:
-            C date;
-            std::string str;
-          };
-          /// inner class end --
-
-          std::list<Calendar<C>::Event> events;
-          
-        protected:
-          C current_date;
+    private:
+      C date;
+      std::string str;
     };
+    /// inner class end --
+
+    std::list<Calendar<C>::Event> all_events() {
+      return this->events;
+    }
+    
+    std::list<Calendar<C>::Event> date_events () {
+      std::list<Calendar::Event> ll;
+      typename std::list<Calendar<C>::Event>::const_iterator it = 
+        events.begin();
+      for (; it != events.end(); it++ ) {
+        Calendar<C>::Event event = *it;
+        if ( event.event_date() > this->current_date ) 
+          ll.push_back(event);
+      }
+      return ll;
+    }
+
+
+
+    std::list<Calendar<C>::Event> events;
+
+    friend std::ostream& operator<<(std::ostream& ostream,
+                                    const Calendar<C>& d ) {
+      typename std::list<Calendar<C>::Event>::const_iterator it =
+        d.events.begin();
+      for ( ; it != d.events.end() ; it++ ) {
+        Calendar<C>::Event event = *it;
+        if (event.event_date() > d.current_date ) {
+          ostream << event << std::endl;
+        }
+      }
+      return ostream;
+    }
+
+  protected:
+    C current_date;
+  };
 }
 
 
--- cpp-code/date/tests/calendar/test.cpp
+++ cpp-code/date/tests/calendar/test.cpp
@@ -9,36 +9,37 @@ using namespace lab2;
 int 
 main ( int argc, char ** argv ) 
 {
-  // time_t mytime;
-  //set_k_time(mytime);
   set_k_time(NULL);
   
   Calendar<Gregorian> cal;
   cal.set_date(2000, 12, 2);
   std::cout << cal.get_date() << std::endl;
   cal.add_event("Basketträning", 4, 12, 2000);
-  cal.add_event("Basketträning", 11, 12, 2000);
   cal.add_event("Nyårsfrukost", 1, 1, 2001);
   cal.add_event("Första advent", 1);          // year = 2000, month = 12
   cal.add_event("Vårdagjämning", 20, 3);      // year  = 2000
   cal.add_event("Julafton", 24, 12);
-  cal.add_event("Kalle Anka hälsar god jul", 24); // också på julafton
-  cal.add_event("Julafton", 24); // En likadan händelse samma datum ska
-                                 // ignoreras och inte sättas in i kalendern
+  cal.add_event("Kalle Anka hälsar god jul", 24); // also on christmas
+  cal.add_event("Julafton", 24); // should be ignored
   cal.add_event("Min första cykel", 20, 12, 2000);
   cal.remove_event("Basketträning", 4);
-  std::cout << cal; // OBS! Vårdagjämning och första advent är
-                    // före nuvarande datum och skrivs inte ut
-  std::cout << "----------------------------------------" << std::endl;
-  cal.remove_event("Vårdagsjämning", 20, 3, 2000);
-  cal.remove_event("Kalle Anka hälsar god jul", 24, 12, 2000);
-  cal.set_date(2000, 11, 2);
-  if (! cal.remove_event("Julafton", 24)) {
-    std::cout << " cal.remove_event(\"Julafton\", 24) tar inte"<< std::endl
-              << " bort något eftersom aktuell månad är november" << std::endl;
-  }
-  std::cout << "----------------------------------------" << std::endl;
+  // std::cout << cal; // OBS! Vårdagjämning och första advent är
+  //                   // före nuvarande datum och skrivs inte ut
+  // std::cout << "----------------------------------------" << std::endl;
+  // cal.remove_event("Vårdagsjämning", 20, 3, 2000);
+  // cal.remove_event("Kalle Anka hälsar god jul", 24, 12, 2000);
+  // cal.set_date(2000, 11, 2);
+  // if (! cal.remove_event("Julafton", 24)) {
+  //   std::cout << " cal.remove_event(\"Julafton\", 24) tar inte"<< std::endl
+  //             << " bort något eftersom aktuell månad är november" << std::endl;
+  // }
+  // std::cout << "----------------------------------------" << std::endl;
   std::cout << cal;
+
+  list<Calendar<Gregorian>::Event> ll = cal.date_events();
+  for ( auto a : ll )
+    cout << "Event:" << a << endl;
+  // }
   
 
 }
