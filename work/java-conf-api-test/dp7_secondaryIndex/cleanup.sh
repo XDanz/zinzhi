95ddcaaccc1b9a3f88150ad6affb8219ce8b3d4e
Daniel Terranova <daniel.terranova@gmail.com>
2015-03-24 23:20:04 +0100

    Removed java-conf-api-test

--- work/java-conf-api-test/dp7_secondaryIndex/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. ../include.sh
-
-kill -9 `ps -eo pid,args | sort | grep -i java | grep -i SimpleWithTrans | awk '{print $1}'` || true
-
-exit $res
--- work/java-conf-api-test/maapi1/mtest.js
+++
@@ -1,8 +0,0 @@
-var validate_int = {
-  validate: function(ctx, path, val) {
-    if (val > 100)
-      return ["warning","must be less than 100"];
-    else
-      return true;
-  }
-};
