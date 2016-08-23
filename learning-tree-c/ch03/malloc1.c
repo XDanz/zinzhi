6cfb8219e39d5cda88c0c64acfabcf4fcdd1fa58
XDanz <daniel.terranova@gmail.com>
2016-08-23 21:50:59 +0200

    Some files

--- learning-tree-c/ch03/malloc1.c
+++ learning-tree-c/ch03/malloc1.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int 
+int
 main (int argc, char** argv)
 {
   char *name; // array of characters
-  int  len;   // length of name
+  size_t  len;   // length of name
   int i;      // loop counter
 
-  scanf("%d", &len );
+  printf ("enter number:");
+  scanf("%zu", &len);
   while ( getchar() != '\n')  // throw away rest of line
-    ; 
+    ;
 
-  name = malloc(len + 1);
-  
+  name = (char*)malloc(len + 1);
+
   if (name == 0) {
-    puts("malloc failed");
-    exit(1);
-  } else {
-    for ( i = 0; i < len; i++ )
-      name[i] = getchar();
-    name[len] = '\0';
+      puts("malloc failed!!");
+      exit(1);
+    } else
+    {
+      printf("Enter %zu chars:", len);
+      for (i = 0; i < len; i++)
+        {
+          name[i] = (char) getchar ();
+        }
+      name[len] = '\0';
 
-    printf ("Goodbye , %s\n", name);
-
-  }
+      printf ("Goodbye , %s\n", name);
+    }
   return 0;
 }
